2f24b0c95ae1e8792b54b719b8b00d77df6ba405
Dalena Trinh <dalenatrinh@Dalenas-MacBook-Air.local>
2023-11-09 16:39:23 +0100

    Galleri opsat + dbclick på hjerte

--- myGallery/src/components/ArtGallery.vue
+++ myGallery/src/components/ArtGallery.vue
@@ -1,37 +1,43 @@
 <script setup>
 
+const imagesData = [
+  new URL('../assets/flower-sun.jpg',import.meta.url).href,
+  new URL('../assets/hands-up.jpg',import.meta.url).href,
+  new URL('../assets/ice-berg.jpg',import.meta.url).href,
+  new URL('../assets/man-in-sand.jpg',import.meta.url).href,
+  new URL('../assets/more-sand.jpg',import.meta.url).href,
+  new URL('../assets/walking-on-rock.jpg',import.meta.url).href,
+];
+
+// Coloring heart with double click
+let isHeartFilled = false;
+// Toggle heart click
+function handleHeartClick() {
+    if(isHeartFilled = false) {
+        isHeartFilled.style.fill = 'red';
+        isHeartFilled = true;
+        isHeartFilled = !isHeartFilled;
+        
+    } else {
+        alert("error")
+    }
+
+};
 
 
 </script>
 
+
 <template>
     <div class="artContainer">
-        <div class="imageContainer">
-            <img class="heart" src="../assets/heart.svg">
-            <img src="../assets/flower-sun.jpg" class="images">
-        </div>
-        <div class="imageContainer">
-            <img class="heart" src="../assets/heart.svg">
-            <img src="../assets/hands-up.jpg" class="images">
-        </div>
-        <div class="imageContainer">
-            <img class="heart" src="../assets/heart.svg">
-            <img src="../assets/man-in-sand.jpg" class="images">
-        </div>
-        <div class="imageContainer">
-            <img class="heart" src="../assets/heart.svg">
-            <img src="../assets/more-sand.jpg" class="images">
-        </div>
-        <div class="imageContainer">
-            <img class="heart" src="../assets/heart.svg">
-            <img src="../assets/walking-on-rock.jpg" class="images">
-        </div>
-        <div class="imageContainer">
-            <img class="heart" src="../assets/heart.svg">
-            <img src="../assets/ice-berg.jpg" class="images">
+        <div v-for="img in imagesData" class="imageContainer">
+        <img :src="img" class="images">
+        <img :style="{filter: filterStyle}" src="../assets/heart.svg" class="heart" @dblclick="handleHeartClick()">
         </div>
     </div>
 </template>
+
+
 
 <style>
 .artContainer {
