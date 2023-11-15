3abdadc9be5e72716cbac817620f608ba9a2e8db
Dalena Trinh <dalenatrinh@Dalenas-MacBook-Air.local>
2023-11-15 16:00:55 +0100

    Push fra mac til pc

--- myGallery/src/components/ArtGallery.vue
+++ myGallery/src/components/ArtGallery.vue
@@ -1,5 +1,7 @@
 <script setup>
+import {ref} from 'vue';
 
+// Billeder
 const imagesData = [
   new URL('../assets/flower-sun.jpg',import.meta.url).href,
   new URL('../assets/hands-up.jpg',import.meta.url).href,
@@ -9,19 +11,13 @@ const imagesData = [
   new URL('../assets/walking-on-rock.jpg',import.meta.url).href,
 ];
 
-// Coloring heart with double click
-let isHeartFilled = false;
-// Toggle heart click
-function handleHeartClick() {
-    if(isHeartFilled = false) {
-        isHeartFilled.style.fill = 'red';
-        isHeartFilled = true;
-        isHeartFilled = !isHeartFilled;
-        
-    } else {
-        alert("error")
-    }
+// Object to track the double click state for each image
+const imageDoubleClickState = imagesData.map(() => ref(false));
 
+
+const toggleHeart = (index) => {
+  // Toggle the double click state for the clicked image
+  imageDoubleClickState[index].value = !imageDoubleClickState[index].value;
 };
 
 
@@ -30,12 +26,17 @@ function handleHeartClick() {
 
 <template>
     <div class="artContainer">
-        <div v-for="img in imagesData" class="imageContainer">
+      <div v-for="(img, index) in imagesData" :key="index" class="imageContainer">
         <img :src="img" class="images">
-        <img :style="{filter: filterStyle}" src="../assets/heart.svg" class="heart" @dblclick="handleHeartClick()">
-        </div>
+        <img
+        :class="{'red-filter': imageDoubleClickState[index].value, 'no-filter': !imageDoubleClickState[index].value}"
+        src="../assets/heart.svg"
+        class="heart"
+        @dblclick="() => toggleHeart(index)"
+/>
+      </div>
     </div>
-</template>
+  </template>
 
 
 
@@ -62,6 +63,14 @@ function handleHeartClick() {
     width: 45px;
     top: 80%;
     left: 85%;
+   
+}
+
+.red-filter {
+    filter: invert(43%) sepia(99%) saturate(5996%) hue-rotate(348deg) brightness(98%) contrast(89%);
+}
+
+.no-filter {
     filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(253deg) brightness(105%) contrast(101%);
 }
 </style>
